82f74df825a28604a7bd394eb3b34b1b39bd4d12
Brandon Dyer <brandondyer64@gmail.com>
2022-12-30 17:07:55 -0600

    Removed 3D scene

--- examples/shared_resources/shared_resources/src/main.rs
+++ examples/shared_resources/shared_resources/src/main.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{log::LogPlugin, prelude::*};
 use bevy_wasm::prelude::*;
 use shared_resources_protocol::{HostMessage, ModMessage, MyCoolResource, PROTOCOL_VERSION};
 
@@ -6,7 +6,8 @@ pub static MOD_WASM: &[u8] = include_bytes!(concat!(env!("OUT_DIR"), "/shared_re
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugin(LogPlugin::default())
+        .add_plugins(MinimalPlugins)
         .insert_resource(MyCoolResource {
             value: 0,
             string: "Hello from MyCoolResource!".to_string(),
@@ -16,42 +17,12 @@ fn main() {
                 .share_resource::<MyCoolResource>(),
         )
         .add_startup_system(insert_mods)
-        .add_startup_system(setup)
         .add_system(update_resource)
         .run();
 }
 
 fn insert_mods(mut wasm: ResMut<WasmResource<HostMessage, ModMessage>>) {
     wasm.insert_wasm(MOD_WASM);
-}
-
-/// set up a simple 3D scene
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    // plane
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
-        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
-        ..default()
-    });
-    commands.spawn(PointLightBundle {
-        point_light: PointLight {
-            intensity: 1500.0,
-            shadows_enabled: true,
-            ..default()
-        },
-        transform: Transform::from_xyz(4.0, 8.0, 4.0),
-        ..default()
-    });
-    // camera
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(-2.0, 3.5, 5.0)
-            .looking_at(Vec3::new(0.0, 1.0, 0.0), Vec3::Y),
-        ..default()
-    });
 }
 
 fn update_resource(mut my_cool_resource: ResMut<MyCoolResource>) {
